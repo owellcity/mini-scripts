2f87f6a72a1948126933178cbcb22618d644b9ab
owellcity <13619139+owellcity@users.noreply.github.com>
2023-09-08 02:53:55 -0700

    Update bridge_pay_payslip_downloader.py

--- bridge_pay_payslip_downloader.py
+++ bridge_pay_payslip_downloader.py
@@ -1,10 +1,15 @@
+# Python 3.7.3
 import requests
 import json
+import time
+
+# Downloads payslips from bridgepay
 
 # Creates Login Session
 session = requests.Session()
 url_login = "https://opswerks.bridgepayday.ph/api/auth/login"
-payload_creds = json.loads('{"username": "", "password": ""}')
+creds = '{"username": "CHANGEME", "password": "CHANGEME"}' # > CHANGE USER AND PASSWORD
+payload_creds = json.loads(creds)
 headers = {'Accept':'application/json, text/plain, */*',
          'Accept-Encoding': 'gzip, deflate, br',
          'Accept-Language': 'en-US,en;q=0.5',
@@ -13,31 +18,41 @@ headers = {'Accept':'application/json, text/plain, */*',
 
 print("Opening session to {}".format(url_login))
 login_session = session.post(url=url_login, json=payload_creds, headers=headers)
+time.sleep(3)
+
+# Appending Authentication Bearer token to headers, used for session get requests
+headers['Authorization'] = "Bearer {}".format(login_session.json()['token'])
+
+# Getting Employee info
+print("Getting employee info..")
+url_employees = "https://opswerks.bridgepayday.ph/api/employees"
+employee_info = session.get(url=url_employees, headers=headers)
+employee_id = employee_info.json()[0]["employeeId"]
+time.sleep(6)
+
+# Getting list of payrolls
+print("Getting list of payrolls..")
+while True:
+    url_payslips = "https://opswerks.bridgepayday.ph/api/view-models/payslips-list?$filter=employeeId%20eq%20{}&$orderby=dateEnd%20desc".format(employee_id)
+    payslips_info = session.get(url=url_payslips, headers=headers)
+    if payslips_info.status_code == 200:
+        break
+    time.sleep(3)
+    print("Retrying: {}".format(url_payslips)) # for somereason, it needs to retry atleast once
 
 # Download payslips
 print("Downloading payslips..")
 url_payslip = "https://opswerks.bridgepayday.ph/api/ssrs-reports/download"
-payroll_id = 1
-while payroll_id < 32:
-    # todo, make payload_request dynamic to anyone
-    payload_request = {"reportType":"OPSWERKS/Reports/Payslip","parameters":"&payroll_id=1&employee_id=xx","exportType":"PDF","filename":"Ramos, Guiller Calmerin","password":"","employeeId":xx}
-    payload_request["parameters"] = "&payroll_id={}&employee_id=xx".format(payroll_id)
+
+for payslip in payslips_info.json():
+    payload_request = {"reportType":"OPSWERKS/Reports/Payslip","parameters":"&payroll_id={}&employee_id={}".format(payslip['payrollId'], employee_id),"exportType":"PDF","filename":"","password":"","employeeId":employee_id}
     payload_request = json.loads(json.dumps(payload_request))
 
-    headers = {'Accept':'application/json, text/plain, */*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Authorization': 'Bearer {}'.format(login_session.json()['token']),
-            'Content-Type': 'application/json'}
-
     payslip_session = session.post(url=url_payslip, json=payload_request, headers=headers)
-    # Captures data stream and compiles it
     if payslip_session.status_code == 200:
-        filename = "payroll_{}.pdf".format(payroll_id)
+        filename = payslip['payrollCode']
         with open(filename, "wb") as f:
             for chunk in payslip_session.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
         print("{}\tDownloaded..".format(filename))
-
-    payroll_id += 1
